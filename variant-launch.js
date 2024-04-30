cd4c624f5975bafbb310e929b624311e7f87fb27
SirForteske <sir.forteske@gmail.com>
2024-04-30 09:42:09 +0000

    variant launcher

---
+++ variant-launch.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = 'https://launchar.app/sdk/v1?key=4tbNlZZs2g9u8eG9nNr7nwUKuJGyHU8W&redirect=true'
+});
